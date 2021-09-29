aa4833832f9c31bb4e8c8b25da77b8e68ea9be6f
Krystian Huang <krystianhuang@gmail.com>
2021-09-29 10:50:49 +0800

    Allow users to send pictures from the local computer library between chats

--- client/src/pages/Chat/index.js
+++ client/src/pages/Chat/index.js
@@ -3,9 +3,10 @@ import PurpleContainer from "../../components/PurpleContainer";
 import WhiteInput from "../../components/WhiteInput";
 import Avatar from '@material-ui/core/Avatar'
 import Grid from "@material-ui/core/Grid";
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import me from '../../assets/imgs/2.jpg'
 import other from '../../assets/imgs/3.jpg'
+import addIcon from '../../assets/imgs/picture_icon.png'
 
 const USER_TYPE = {
 	me: 0,
@@ -15,23 +16,28 @@ const USER_TYPE = {
 const ary = [0, 1]
 
 function Chat() {
+	const imgInputRef = useRef()
 	const [inputValue, setInputValue] = useState('')
 	const [chatList, setChatList] = useState([{
 		id: 1,
 		type: 0,
-		message: 'Hello'
+		message: other,
+		messageType: 'picture'
 	}, {
 		id: 2,
 		type: 1,
-		message: 'Test'
+		message: other,
+		messageType:'picture'
 	}, {
 		id: 3,
 		type: 0,
-		message: 'Test'
+		message: 'Test',
+		messageType: 'text'
 	}, {
 		id: 4,
 		type: 1,
-		message: 'Test'
+		message: 'Test',
+		messageType: 'text'
 	}
 	])
 
@@ -45,11 +51,45 @@ function Chat() {
 		setInputValue(e.target.value)
 	}
 
+	const autoScroll =()=>{
+		const ele = document.querySelector('.chat-container')
+		ele.scrollTop = ele.scrollHeight + 60
+	}
+
 	const onKeyPress = (e) => {
 		if (e.code === 'Enter') {
-			setChatList([...chatList, {id: chatList.length + 1, type: getRandom(), message: inputValue}])
-			const ele = document.querySelector('.chat-container')
-			ele.scrollTop = ele.scrollHeight + 40
+			setChatList([...chatList, {id: chatList.length + 1, type: getRandom(), message: inputValue,messageType: 'text'}])
+			autoScroll()
+		}
+	}
+
+	const onAddImg = ()=>{
+		imgInputRef.current.click()
+	}
+
+	const transformFileToBase64 = (file) => {
+		return new Promise(reslove => {
+			const reader = new FileReader()
+			reader.readAsDataURL(file)
+			reader.onload = () => {
+				reslove(reader.result)
+			}
+		})
+	}
+
+
+
+	const onUploadChange = async (e) => {
+		if (e.target.files) {
+			const [file] = e.target.files
+			if(!file) return
+			const fileSuffix = file.name.substring(file.name.lastIndexOf('.') + 1)
+			if (!['jpg', 'png', 'jpeg','gif'].includes(fileSuffix)) {
+				return
+			}
+			const imgUrl =await transformFileToBase64(file)
+			setChatList([...chatList, {id: chatList.length + 1, type: USER_TYPE.me, message: imgUrl,messageType: 'picture'}])
+			autoScroll()
 		}
 	}
 
@@ -67,11 +107,19 @@ function Chat() {
 						chatList.map(v => <Grid className='chat-item' key={v.id} item xs={12}>
 								{
 									v.type === USER_TYPE.me ? <div className='me'>
-										<div className='me-message'>{v.message}</div>
+										{
+											v.messageType==='picture'?<img src={v.message} className='chat-img-me'/>:
+												<div className='me-message'>{v.message}</div>
+										}
+
 										<Avatar src={me}/>
 									</div> : <div className='other'>
 										<Avatar src={other}/>
-										<div className='other-message'>{v.message}</div>
+
+										{
+											v.messageType==='picture'?<img src={v.message} className='chat-img'/>:
+												<div className='other-message'>{v.message}</div>
+										}
 									</div>
 								}
 							</Grid>
@@ -79,7 +127,14 @@ function Chat() {
 					}
 				</div>
 				<Grid item xs={12}>
-					<WhiteInput value={inputValue} onKeyPress={onKeyPress} onChange={onChange}/>
+					<div className='input-wrapper'>
+						<WhiteInput className='chat-input' value={inputValue} onKeyPress={onKeyPress} onChange={onChange}/>
+						<img src={addIcon} onClick={onAddImg} className='add-icon'/>
+					</div>
+
+					<input ref={imgInputRef} type='file' style={{display:'none'}}
+						   onChange={onUploadChange}
+					/>
 				</Grid>
 			</Grid>
 		</PurpleContainer>
