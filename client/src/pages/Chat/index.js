e3ac9d0a157b23334efd2c18dbb48e21dadc9d1c
Krystian Huang <krystianhuang@gmail.com>
2021-10-05 20:39:05 +0800

    1. Add Emoji function to the chat page. 2. Click on the picture in the chat to zoom in and pop up on the screen.

--- client/src/pages/Chat/index.js
+++ client/src/pages/Chat/index.js
@@ -4,9 +4,15 @@ import WhiteInput from "../../components/WhiteInput";
 import Avatar from '@material-ui/core/Avatar'
 import Grid from "@material-ui/core/Grid";
 import React, {useRef, useState} from "react";
-import me from '../../assets/imgs/2.jpg'
-import other from '../../assets/imgs/3.jpg'
-import addIcon from '../../assets/imgs/picture_icon.png'
+import me from '../../assets/imgs/chat-picture1.jpg'
+import other from '../../assets/imgs/chat-picture2.jpg'
+import addIcon from '../../assets/imgs/002.jpg'
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import {withStyles} from '@material-ui/core/styles';
+import iconSmile from '../../assets/imgs/001.jpg'
+import {Picker} from 'emoji-mart'
+import 'emoji-mart/css/emoji-mart.css'
 
 const USER_TYPE = {
 	me: 0,
@@ -14,6 +20,13 @@ const USER_TYPE = {
 }
 
 const ary = [0, 1]
+
+const DialogContent = withStyles((theme) => ({
+	root: {
+		padding: theme.spacing(2),
+	},
+}))(MuiDialogContent);
+
 
 function Chat() {
 	const imgInputRef = useRef()
@@ -27,7 +40,7 @@ function Chat() {
 		id: 2,
 		type: 1,
 		message: other,
-		messageType:'picture'
+		messageType: 'picture'
 	}, {
 		id: 3,
 		type: 0,
@@ -40,6 +53,9 @@ function Chat() {
 		messageType: 'text'
 	}
 	])
+	const [open, setOpen] = useState(false)
+	const [bigImgUrl, setBigImgUrl] = useState('')
+	const [showEmoji, setShowEmoji] = useState(false)
 
 	const getRandom = () => {
 		const l = Math.floor(Math.random() * ary.length)
@@ -51,19 +67,26 @@ function Chat() {
 		setInputValue(e.target.value)
 	}
 
-	const autoScroll =()=>{
+	const autoScroll = () => {
 		const ele = document.querySelector('.chat-container')
-		ele.scrollTop = ele.scrollHeight + 60
+		setTimeout(() => {
+			ele.scrollTop = ele.scrollHeight + 60
+		}, 200)
 	}
 
 	const onKeyPress = (e) => {
 		if (e.code === 'Enter') {
-			setChatList([...chatList, {id: chatList.length + 1, type: getRandom(), message: inputValue,messageType: 'text'}])
+			setChatList([...chatList, {
+				id: chatList.length + 1,
+				type: getRandom(),
+				message: inputValue,
+				messageType: 'text'
+			}])
 			autoScroll()
 		}
 	}
 
-	const onAddImg = ()=>{
+	const onAddImg = () => {
 		imgInputRef.current.click()
 	}
 
@@ -78,66 +101,109 @@ function Chat() {
 	}
 
 
-
 	const onUploadChange = async (e) => {
 		if (e.target.files) {
 			const [file] = e.target.files
-			if(!file) return
+			if (!file) return
 			const fileSuffix = file.name.substring(file.name.lastIndexOf('.') + 1)
-			if (!['jpg', 'png', 'jpeg','gif'].includes(fileSuffix)) {
+			if (!['jpg', 'png', 'jpeg', 'gif'].includes(fileSuffix)) {
 				return
 			}
-			const imgUrl =await transformFileToBase64(file)
-			setChatList([...chatList, {id: chatList.length + 1, type: USER_TYPE.me, message: imgUrl,messageType: 'picture'}])
+			const imgUrl = await transformFileToBase64(file)
+			setChatList([...chatList, {
+				id: chatList.length + 1,
+				type: USER_TYPE.me,
+				message: imgUrl,
+				messageType: 'picture'
+			}])
 			autoScroll()
 		}
 	}
 
+	const handleClose = () => {
+		setOpen(false)
+	}
+
+
+	const showDialog = (imgUrl) => {
+		setOpen(true)
+		setBigImgUrl(imgUrl)
+	}
+
+	const choiceEmoji = (emoji, event) => {
+		setChatList([...chatList, {
+			id: chatList.length + 1,
+			type: getRandom(),
+			message: emoji.native,
+			messageType: 'emoji'
+		}])
+		autoScroll()
+	}
+
 
 	return (
-		<PurpleContainer width={600}>
-			<Grid container spacing={3}>
-				<Grid item xs={12}>
-					<h2 className="title">
-						name
-					</h2>
-				</Grid>
-				<div className='chat-container'>
-					{
-						chatList.map(v => <Grid className='chat-item' key={v.id} item xs={12}>
-								{
-									v.type === USER_TYPE.me ? <div className='me'>
-										{
-											v.messageType==='picture'?<img src={v.message} className='chat-img-me'/>:
-												<div className='me-message'>{v.message}</div>
-										}
+		<>
+			<PurpleContainer width={600}>
+				<Grid container spacing={3}>
+					<Grid item xs={12}>
+						<h2 className="title">
+							Chat
+						</h2>
+					</Grid>
+					<div className='chat-container'>
+						{
+							chatList.map(v => <Grid className='chat-item' key={v.id} item xs={12}>
+									{
+										v.type === USER_TYPE.me ? <div className='me'>
+											{
+												v.messageType === 'picture' ?
+													<img onClick={() => showDialog(v.message)} src={v.message}
+														 className='chat-img-me'/> :
+													<div
+														className={`${v.messageType === 'emoji' ? 'me-message-emoji' : 'me-message'}`}>{v.message}</div>
+											}
 
-										<Avatar src={me}/>
-									</div> : <div className='other'>
-										<Avatar src={other}/>
+											<Avatar src={me}/>
+										</div> : <div className='other'>
+											<Avatar src={other}/>
 
-										{
-											v.messageType==='picture'?<img src={v.message} className='chat-img'/>:
-												<div className='other-message'>{v.message}</div>
-										}
-									</div>
-								}
-							</Grid>
-						)
-					}
-				</div>
-				<Grid item xs={12}>
-					<div className='input-wrapper'>
-						<WhiteInput className='chat-input' value={inputValue} onKeyPress={onKeyPress} onChange={onChange}/>
-						<img src={addIcon} onClick={onAddImg} className='add-icon'/>
+											{
+												v.messageType === 'picture' ?
+													<img onClick={() => showDialog(v.message)} src={v.message}
+														 className='chat-img'/> :
+													<div
+														className={`${v.messageType === 'emoji' ? 'other-message-emoji' : 'other-message'}`}>{v.message}</div>
+											}
+										</div>
+									}
+								</Grid>
+							)
+						}
 					</div>
+					<Grid item xs={12}>
+						<div className='input-wrapper'>
+							<WhiteInput className='chat-input' value={inputValue} onKeyPress={onKeyPress}
+										onChange={onChange}/>
+							<img src={iconSmile} onClick={() => setShowEmoji(!showEmoji)} className='add-icon'/>
+							<img src={addIcon} onClick={onAddImg} className='add-icon'/>
+						</div>
 
-					<input ref={imgInputRef} type='file' style={{display:'none'}}
-						   onChange={onUploadChange}
-					/>
+						{
+							showEmoji ?
+								<Picker set='apple' inclde={['people']}  showPreview={false} showSkinTones={false}  onClick={choiceEmoji}/> : null
+						}
+						<input ref={imgInputRef} type='file' style={{display: 'none'}}
+							   onChange={onUploadChange}
+						/>
+					</Grid>
 				</Grid>
-			</Grid>
-		</PurpleContainer>
+
+			</PurpleContainer>
+
+			<Dialog maxWidth={800} onClose={handleClose} open={open}>
+				<img src={bigImgUrl} className='big-img-url'/>
+			</Dialog>
+		</>
 	)
 }
 
