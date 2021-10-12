53ba3da6a08150ac544b4df880e2e484e4366966
Nick Georgiou <nickg1950@hotmail.com>
2021-10-12 23:04:19 +1100

    clean up

---
+++ client/src/components/MachineLearning/MachineLearningDashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function MachineLearningDashboard(){
+	return (
+		<div className="box">
+			<div className="text1">
+				Clusters
+			</div>
+		</div>
+	);
+}
+
+export default MachineLearningDashboard;
